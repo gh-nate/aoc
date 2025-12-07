f6dc4485d0e43ac53f0020c00b55e50721e0be5f
gh-nate <122752052+gh-nate@users.noreply.github.com>
2025-12-06 21:17:03 -0800

    Add solutions for the 2015 day 3 puzzle

---
+++ 2015/day03/main.go
@@ -0,0 +1,83 @@
+// Copyright (c) 2025 gh-nate
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type point struct {
+	x int
+	y int
+}
+
+func main() {
+	data, _ := os.ReadFile(os.Args[len(os.Args)-1])
+
+	var santa point
+	visited := map[point]bool{santa: true}
+	for _, b := range data {
+		switch b {
+		case '^':
+			santa.y += 1
+		case 'v':
+			santa.y -= 1
+		case '>':
+			santa.x += 1
+		case '<':
+			santa.x -= 1
+		}
+		visited[santa] = true
+	}
+
+	fmt.Println("Part 1:", len(visited))
+
+	clear(visited)
+	santa.x = 0
+	santa.y = 0
+	visited[santa] = true
+	var (
+		robot point
+		who   *point
+	)
+
+	for i, b := range data {
+		if i%2 == 0 {
+			who = &santa
+		} else {
+			who = &robot
+		}
+		switch b {
+		case '^':
+			who.y += 1
+		case 'v':
+			who.y -= 1
+		case '>':
+			who.x += 1
+		case '<':
+			who.x -= 1
+		}
+		visited[*who] = true
+	}
+
+	fmt.Println("Part 2:", len(visited))
+}
